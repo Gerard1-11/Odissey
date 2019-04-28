9a564b7ad3ec1054932fa33a8c8f742720061494
Gerard1-11 <A01374994@itesm.mx>
2019-04-28 09:14:14 -0500

    El terminadito???

---
+++ app/src/main/java/mx/dbas/odissey/Visor.java
@@ -0,0 +1,37 @@
+package mx.dbas.odissey;
+import mx.dbas.*;
+
+import com.google.common.collect.ComputationException;
+
+import java.io.IOException;
+
+public class Visor {
+    String projectID="hackmx2-238923\n";
+    String computeRegion="";
+    String modelId = "SalonCongresos_v20190428072703";
+    String filePath;
+    String scoreThreshold="90";
+
+    PredictionApi pred;
+
+    public void main(String[] args){
+
+
+        prediction(filePath);
+
+
+
+    }
+
+
+    private void prediction(String filepath){
+        try {
+            pred.predict(projectID, computeRegion, modelId, filePath, scoreThreshold);
+
+        }catch (IOException e){
+            System.out.println("Unreachable path");
+
+        }
+    }
+
+}
