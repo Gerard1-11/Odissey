c32fcda003c53a69b0b98f6badd320b5a70d3503
Gerard1-11 <A01374994@itesm.mx>
2019-04-28 05:19:42 -0500

    la interfazZ???

---
+++ app/src/main/java/mx/dbas/odissey/SplashScreen.java
@@ -0,0 +1,33 @@
+package mx.dbas.odissey;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static java.lang.Thread.sleep;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+        Thread MyThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        MyThread.start();
+    }
+}
